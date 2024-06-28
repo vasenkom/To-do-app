a17dee4ca47495f9c07d1a8bef046a18c103dfc3
vasenkom <m.vasenko@yandex.ru>
2024-06-28 23:24:42 +0200

    try to insert img - not successful

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import "./style.css";
 import { greetFunction } from "./greetings.js";
 import { createAddTaskDialog } from "./dialog.js";
 import { createTask } from "./newToDo.js";
+import star from "./star.png";
 
 export const contentBox = document.querySelector("#content");
 const addTodoButton = document.querySelector(".addButton");
+
+// // Add images to menu buttons
+// const todayButton = document.querySelector(".todaySortButton");
+// const starImage = document.createElement("img");
+// starImage.src = star;
+// todayButton.appendChild(starImage);
 
 // Initialize the greeting
 greetFunction();
