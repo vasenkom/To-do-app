a43668e18679876c693097a6acdf86604a3feabc
vasenkom <m.vasenko@yandex.ru>
2024-06-29 13:03:01 +0200

    add code to add img to menu buttons

--- src/index.js
+++ src/index.js
@@ -4,15 +4,32 @@ import { greetFunction } from "./greetings.js";
 import { createAddTaskDialog } from "./dialog.js";
 import { createTask } from "./newToDo.js";
 import star from "./star.png";
+import add from "./plus4.png";
+import allPapers from "./allTasks.png";
 
 export const contentBox = document.querySelector("#content");
 const addTodoButton = document.querySelector(".addButton");
 
-// // Add images to menu buttons
-// const todayButton = document.querySelector(".todaySortButton");
-// const starImage = document.createElement("img");
-// starImage.src = star;
-// todayButton.appendChild(starImage);
+// Add star img to Today button
+const imgholderAdd = document.querySelector(".imgholderAdd");
+const addImage = document.createElement("img");
+addImage.classList.add("addImage");
+addImage.src = add;
+imgholderAdd.appendChild(addImage);
+
+// Add star img to Today button
+const imgholderStar = document.querySelector(".imgholderStar");
+const starImage = document.createElement("img");
+starImage.classList.add("starImage");
+starImage.src = star;
+imgholderStar.appendChild(starImage);
+
+// Add star img to Today button
+const imgholderAll = document.querySelector(".imgholderAll");
+const allTasksImage = document.createElement("img");
+allTasksImage.classList.add("allTasksImage");
+allTasksImage.src = allPapers;
+imgholderAll.appendChild(allTasksImage);
 
 // Initialize the greeting
 greetFunction();
