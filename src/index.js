324d70dab6dc7e89de6d40f692d35b23afdecc33
vasenkom <m.vasenko@yandex.ru>
2024-07-03 23:50:00 +0200

    qork on code disign

--- src/index.js
+++ src/index.js
@@ -1,43 +1,26 @@
-import _ from "lodash";
 import "./style.css";
+import { addImagesToButtons } from "./DOM.js";
 import { greetFunction } from "./greetings.js";
+import { addDefaultTask } from "./defaultTask.js";
+import { setupEventListeners } from "./eventListeners.js";
+import { addCheckboxListeners } from "./eventListeners.js";
 import { createAddTaskDialog } from "./dialog.js";
-import { createTask } from "./newToDo.js";
-import star from "./star.png";
-import add from "./plus4.png";
-import allPapers from "./allTasks.png";
+import {
+  contentBox,
+  addTodoButton,
+  addTodoButton2,
+  todaySortButton,
+  allTasksButton,
+  checkBoxM,
+} from "./domElements.js";
 
-export const contentBox = document.querySelector("#content");
-const addTodoButton = document.querySelector(".addButton");
-
-// Add star img to Today button
-const imgholderAdd = document.querySelector(".imgholderAdd");
-const addImage = document.createElement("img");
-addImage.classList.add("addImage");
-addImage.src = add;
-imgholderAdd.appendChild(addImage);
-
-// Add star img to Today button
-const imgholderStar = document.querySelector(".imgholderStar");
-const starImage = document.createElement("img");
-starImage.classList.add("starImage");
-starImage.src = star;
-imgholderStar.appendChild(starImage);
-
-// Add star img to Today button
-const imgholderAll = document.querySelector(".imgholderAll");
-const allTasksImage = document.createElement("img");
-allTasksImage.classList.add("allTasksImage");
-allTasksImage.src = allPapers;
-imgholderAll.appendChild(allTasksImage);
+addImagesToButtons();
 
 // Initialize the greeting
 greetFunction();
 
-// Create and append the addTaskDialogHTML
-const addTaskDialogHTML = createAddTaskDialog(contentBox, addTodoButton);
+// Adds default task
+addDefaultTask();
 
-// Add event listener to open dialog for task adding
-addTodoButton.addEventListener("click", () => {
-  addTaskDialogHTML.showModal();
-});
+setupEventListeners();
+addCheckboxListeners();
