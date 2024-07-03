8917c0085f39e97b7241f45ef0dd01bcc4c7292b
vasenkom <m.vasenko@yandex.ru>
2024-07-03 23:48:59 +0200

    create file with all important dom elements

---
+++ src/domElements.js
@@ -0,0 +1,6 @@
+export const contentBox = document.querySelector("#content");
+export const addTodoButton = document.querySelector(".addButton");
+export const addTodoButton2 = document.querySelector(".addButton2");
+export const todaySortButton = document.querySelector(".todaySortButton");
+export const allTasksButton = document.querySelector(".allTasksButton");
+export const checkBoxM = document.querySelectorAll("#doneStatus");
